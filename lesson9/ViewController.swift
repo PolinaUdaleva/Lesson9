bd47f9cba74050f5036c0eb7c2ba5f9b6e1ed3b1
Polina Udaliova <udaliovapolina@gmail.com>
2022-09-28 22:49:24 +0300

    add square

--- lesson9/ViewController.swift
+++ lesson9/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    let mySquare = UIView()
+    let firstSquare = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(self.gestureRec))
+        let click = UITapGestureRecognizer(target: self, action: #selector(self.gestureRecSquare))
+        firstSquare.addGestureRecognizer(click)
+
+        view.addGestureRecognizer(gesture)
+        view.addSubview(firstSquare)
     }
+    
+    @objc func gestureRec() {
+        let widthView = view.frame.size.width
+        let heightView = view.frame.size.height
+        mySquare.removeFromSuperview()
+        
+        firstSquare.backgroundColor = UIColor(
+        red: CGFloat(Int.random(in: 1..<255))/255,
+        green: CGFloat(Int.random(in: 1..<255))/255,
+        blue: CGFloat(Int.random(in: 1..<255))/255,
+        alpha: 1)
+        firstSquare.snp.remakeConstraints{ (make) -> Void in
+            make.width
+                .height
+                .equalTo(200)
+            make.top.equalTo(view).offset(Int.random(in: 0..<Int((widthView - 200))))
+            make.left.equalTo(view).offset(Int.random(in: 0..<Int((heightView - 200))))
+        }
+        view.addSubview(firstSquare)
+    }
+    @objc func gestureRecSquare() {
+        firstSquare.addSubview(mySquare)
+        mySquare.backgroundColor = .green
+        mySquare.snp.remakeConstraints{ make in
+            make.width
+                .height
+                .equalTo(50)
 
-
+        }
+    }
 }
-
